f72e34b1ebb574b5e0c15eee077019e712f3d53f
Isadora Chieregato <chieregatoisadora@gmail.com>
2025-05-14 00:44:16 -0300

    Initial business card

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const showGit = () => {
+    setTimeout(() => {
+        window.location.href = "https://github.com/isadorachieregato";
+    }, 500);
+};
+
+const showLinkedin = () => {
+    setTimeout(() => {
+        window.location.href = "https://www.linkedin.com/in/isadorachieregato/";
+    }, 500);
+};
+
+const showWhats = () => {
+    setTimeout(() => {
+        window.location.href = "https://wa.me/5516994097027";
+    },500);
+};
+const showEmail  = () => {
+    setTimeout(() => {
+        window.location.href = "mailto:isadorachieregato@outlook.com";
+    }, 500);
+};
